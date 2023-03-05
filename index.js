7fc85f40f39d3d832dba5382f3084cb98e636cfa
guilherme <guilherme.a.caldas2017jj@gmail.com>
2023-03-05 18:37:09 -0300

    add rua do cep na funçao

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const i=document.querySelector('.i')
 const b =document.querySelector('.b')
+const d =document.querySelector('.d')
 
 const fecthApi=async(cep)=>{
     
     const dado =await fetch(`https://viacep.com.br/ws/${cep}/json/`)
     const response =await dado.json()
     console.log(response);
+
+    const streetTxt=document.createElement('p')
+     streetTxt.innerText='rua'
+    d.appendChild(streetTxt)
+    
+    const street=document.createElement('p')
+    street.innerText=response.logradouro
+    d.appendChild(street)
+
+
+
 }
+
+
 
 
 b.addEventListener('click',(e)=>{
