327281ea4347a457db5101f8a241413bb12a9ae2
guilherme <guilherme.a.caldas2017jj@gmail.com>
2023-03-05 17:32:55 -0300

    relacionando input com a funçao

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+const i=document.querySelector('.i')
+const b =document.querySelector('.b')
+
 const fecthApi=async(cep)=>{
+    
     const dado =await fetch(`https://viacep.com.br/ws/${cep}/json/`)
     const response =await dado.json()
     console.log(response);
 }
 
-const input=document.querySelector('.inputBusca')
 
-const botao=document.querySelector('.buscaBotao')
-
-
-
-fecthApi('22020-002')
+b.addEventListener('click',(e)=>{
+    e.preventDefault()
+    fecthApi(i.value)})
